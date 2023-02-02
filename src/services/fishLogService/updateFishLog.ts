997236a1777bbd4d914e8011857f60e9518d27d9
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2023-02-02 18:42:48 -0300

    atualiza funcao de update de fishlog

--- src/services/fishLogService/updateFishLog.ts
+++ src/services/fishLogService/updateFishLog.ts
@@ -3,6 +3,17 @@ import {Buffer} from 'buffer';
 import {storage} from '../../global/config/storage';
 import {fishLogService} from './fishService';
 
+type userStorage = {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+  admin: boolean;
+  superAdmin: boolean;
+  token: string;
+};
+
 async function UpdateFishLog(
   log_id: string,
   name: string | undefined,
@@ -19,11 +30,17 @@ async function UpdateFishLog(
   superAdmin: Boolean,
   visible: boolean,
 ) {
-  const userId = await storage.getString('@eupescador/userId');
-  const token = await storage.getString('@eupescador/token');
+  const user = storage.getString("@eupescador/user");
+  if (!user) return;
+
+  const userAdmin = JSON.parse(user) as userStorage;
+  
+  const token = userAdmin.token;
+  const userId = userAdmin.id;
   const userToken = `Bearer ${token}`;
   let photo = null;
   let reviewedBy = null;
+  console.log("entrou no update")
 
   if (admin || superAdmin) reviewedBy = userId;
 
@@ -34,7 +51,7 @@ async function UpdateFishLog(
   if (photoString) {
     photo = photoString;
   }
-  const res = await fishLogService.patch(
+  const res = await fishLogService.put(
     `/fishLog/${log_id}`,
     {
       name,
@@ -52,6 +69,7 @@ async function UpdateFishLog(
     },
     {headers: {Authorization: userToken}},
   );
+  console.log("atualizou")
   return res.data;
 }
 
